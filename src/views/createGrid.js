aaff357dd9076772544e9e2a1efa175be3805f39
Nthabi2905 <149662900+Nthabi2905@users.noreply.github.com>
2026-05-08 18:55:08 +0200

    Create responsive podcast grid view

---
+++ src/views/createGrid.js
@@ -0,0 +1,8 @@
+import { createPodcastCard } from "../components/createPodcastCard.js";
+
+export function createGrid(container, podcasts, genreService, onSelect) {
+  container.innerHTML = "";
+  podcasts.forEach((podcast) =>
+    container.appendChild(createPodcastCard(podcast, genreService, onSelect)),
+  );
+}
